5a4d61089fe5636a76a2ed104814534fbb26da57
Thunderbeast5 <163726116+Thunderbeast5@users.noreply.github.com>
2026-01-15 09:36:48 +0530

    react icons used

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,9 @@
 import React, { useState } from 'react';
+import { FaUser } from "react-icons/fa"
+import { FaSearch } from "react-icons/fa";
+import { FaCartShopping } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa6";
+import { FaWhatsapp } from "react-icons/fa6";
 
 const Navbar = () => {
   const [isShopOpen, setIsShopOpen] = useState(false);
@@ -21,7 +26,7 @@ const Navbar = () => {
 
       {/* 2. Logo Section - Reduced padding */}
       <div className="flex justify-center py-4">
-        <h1 className="text-3xl md:text-4xl tracking-[0.35em] text-text-primary font-light uppercase">
+        <h1 className="text-3xl md:text-4xl tracking-[0.35em] text-text-primary font-light ">
           mud and moon
         </h1>
       </div>
@@ -42,12 +47,10 @@ const Navbar = () => {
         {/* Socials - Slightly larger icons */}
         <div className="flex gap-5 text-icon">
           <button className="hover:text-primary-button cursor-pointer transition-colors">
-            <i className="fab fa-instagram text-xl"></i> {/* Or SVG */}
-            <span className="sr-only">Instagram</span>
+            <FaInstagram />
           </button>
           <button className="hover:text-primary-button cursor-pointer transition-colors">
-            <i className="fab fa-youtube text-xl"></i>
-            <span className="sr-only">YouTube</span>
+           <FaWhatsapp />
           </button>
         </div>
 
@@ -107,13 +110,13 @@ const Navbar = () => {
         {/* Action Icons - Slightly larger */}
         <div className="flex gap-6 text-text-primary">
           <button className="hover:text-primary-button cursor-pointer transition-all hover:scale-105">
-            <svg className="w-5.5 h-5.5" fill="none" stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24"><path d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z" /></svg>
+            <FaCartShopping />
           </button>
           <button className="hover:text-primary-button cursor-pointer transition-all hover:scale-105">
-            <svg className="w-5.5 h-5.5" fill="none" stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24"><path d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" /></svg>
+            <FaUser />
           </button>
           <button className="hover:text-primary-button cursor-pointer transition-all hover:scale-105">
-            <svg className="w-5.5 h-5.5" fill="none" stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24"><path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+            <FaSearch />
           </button>
         </div>
       </div>
